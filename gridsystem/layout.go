c487abb04d51b13a690f1fcffe83299d9eb477ca
joe-nas <jonas@falcken.de>
2025-03-18 14:05:35 +0100

    playing with grid system

--- gridsystem/layout.go
+++ gridsystem/layout.go
@@ -1,31 +1,69 @@
 package main
 
-type layout struct {
-	layout_matrix [][]string
-}
-type column struct {
-	s   string
-	row int
-	col int
-}
+import (
+	"fmt"
 
-func (l layout) addColumn() {
-	l.layout_matrix = append(l.layout_matrix)
+	"github.com/charmbracelet/lipgloss"
+)
+
+type Layout struct {
+	layout_matrix [][]Column
 }
 
-func (l layout) addRow() {
-
+type Column struct {
+	s string
+	// row int
+	// col int
 }
 
-func createLayout() layout {
-
-	header := []string{"header"}
-	stats := []string{"stat1", "stat2", "stat3"}
-	help := []string{"help"}
-	game := []string{"game"}
-	footer := []string{"footer"}
-
-	return layout{
-		layout_matrix: [][]string{header, stats, help, game, footer},
-	}
+func NewLayout() *Layout {
+	layout := new(Layout)
+	// layout.layout_matrix = make([][]Column, 0)
+	return layout
 }
+
+func (l *Layout) addColumns(columns int) {
+	l.layout_matrix = append(l.layout_matrix, make([]Column, columns))
+}
+
+var (
+	teststyle = lipgloss.NewStyle().
+		Background(lipgloss.Color("#7d33ff"))
+)
+
+func (c *Column) addContentToColumn() {
+	c.s = teststyle.Render("lkgjdlfkgldf")
+}
+
+func main() {
+	var layout = NewLayout()
+	// row0 - 2
+
+	layout.addColumns(0)
+	layout.addColumns(1)
+	layout.addColumns(2)
+	layout.addColumns(10)
+
+	fmt.Print(layout)
+}
+
+// func (l layout) addColumn() {
+// 	// l.layout_matrix = append(l.layout_matrix)
+// }
+
+// func layoutCreate() *layout {
+// 	return &layout{}
+// }
+
+// func createLayout() layout {
+
+// 	header := []string{"header"}
+// 	stats := []string{"stat1", "stat2", "stat3"}
+// 	help := []string{"help"}
+// 	game := []string{"game"}
+// 	footer := []string{"footer"}
+
+// 	return layout{
+// 		layout_matrix: [][]string{header, stats, help, game, footer},
+// 	}
+// }
